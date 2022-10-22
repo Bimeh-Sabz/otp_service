36e1426cb7e6eb5eaf361edaf8cf3f9a9c61e4ba
Hossein Araghi <hoseinaraghi84@gmail.com>
2022-10-22 17:44:14 +0330

    Fix request technology interference

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import os
 
 from dotenv import load_dotenv
 from fastapi import FastAPI
-
 from otp.melipayamak import Api
 
 app = FastAPI()
 load_dotenv()
-api = Api(os.getenv("OTP_USERNAME"), os.getenv("OTP_PASSWORD")).sms("soap")
+apiSoap = Api(os.getenv("OTP_USERNAME"), os.getenv("OTP_PASSWORD")).sms("soap")
+apiRest = Api(os.getenv("OTP_USERNAME"), os.getenv("OTP_PASSWORD")).sms("rest")
+_from = os.getenv("OTP_PHONE_NUMBER")
 
 
 @app.get("/")
