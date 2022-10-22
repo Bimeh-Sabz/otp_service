4c0b33bf9e1c4b7b463e5f214ca6852715bcb01b
Hossein Araghi <hoseinaraghi84@gmail.com>
2022-10-22 17:48:58 +0330

    Create base price & send sms rest api

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-
+import re
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from otp.melipayamak import Api
@@ -23,4 +23,25 @@ async def get_credit():
 
 @app.get("/getBasePrice")
 async def get_base_price():
-    return {"price": api.get_credit()}
+    return apiRest.get_base_price()
+
+
+@app.post("/sendSMS")
+async def send_sms(msg: str = "", number: str = ""):
+    if msg == "":
+        return {"status": "error", "msg": "empty sms body!"}
+    else:
+        if number == "":
+            return {"status": "error", "msg": "empty phone number!"}
+        else:
+            if re.search("^09[0-9][0-9]{8}$", number):
+                res_id = apiSoap.send(number, _from, msg)
+
+                print(f"INFO: SMS send with id {res_id}")
+
+                if apiSoap.is_delivered(res_id):
+                    return {"status": "success", "msg": "done!"}
+                else:
+                    return {"status": "error", "msg": "message not delivered!"}
+            else:
+                return {"status": "error", "msg": "wrong phone number"}
